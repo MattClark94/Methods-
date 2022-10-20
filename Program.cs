6a3218eabfbfb2c48fbf0a16461d111c7f10db6d
Matt.Clark <mc1556@student.aru.ac.uk>
2022-10-20 11:44:39 +0100

    Numbers to words

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int number; 
+
+
+String convert(int number)
+{
+    string[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight","nine"}; 
+
+    int hundreds = number /100; 
+    number = number % 100 ; 
+    int tens = number /10; 
+    number = number % 10; 
+    int 
+
+    return numbers[hundreds -1] + " " + numbers[tens -1] +" "; 
+}
+
+Console.WriteLine("please enter a number bewteen -999 & 999"); 
+number = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine(" Number is " + convert(number)); 
+
+Console.ReadKey(); 
+
+
+
+
+
+
+
+
